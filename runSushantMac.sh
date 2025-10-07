51e4df3047eb9e4c9f8fd9ecb815d69b64b89e46
sushant-bhattarai <sushantb2055@gmail.com>
2025-10-07 17:23:43 -0400

    added a new batch file that runs in Mac Machine

---
+++ runSushantMac.sh
@@ -0,0 +1,4 @@
+python launcher.py -n 128random_diameter7test.edgelist -c 2 -r 50
+python launcher.py -n 256random_diameter5test.edgelist -c 2 -r 50
+python launcher.py -n 512random_diameter4test.edgelist -c 2 -r 50
+python launcher.py -n 1024random_diameter4test.edgelist -c 2 -r 50
