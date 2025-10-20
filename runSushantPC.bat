f4da821796d066d6409d496799d52fa41abb908e
Sushant Bhattarai <sushantb2055@gmail.com>
2025-10-20 11:54:23 -0400

    updated the code for diam/3 criteria and to store into threshEqualsDiamOver3_data

--- runSushantPC.bat
+++ runSushantPC.bat
@@ -1,8 +1,16 @@
 @echo off
 echo Running experiments...
 python launcher.py -n 128random_diameter7test.edgelist -c 2 -r 50
+python launcher.py -n 128random_diameter7test.edgelist -c 3.3333333333333333333333333333333333333333 -r 50
 python launcher.py -n 128random_diameter7test.edgelist -c 10 -r 50
+python launcher.py -n 256random_diameter5test.edgelist -c 2.5 -r 50
+python launcher.py -n 256random_diameter5test.edgelist -c 3.3333333333333333333333333333333333333333 -r 50
 python launcher.py -n 256random_diameter5test.edgelist -c 10 -r 50
+python launcher.py -n 512random_diameter4test.edgelist -c 2 -r 50
+python launcher.py -n 512random_diameter4test.edgelist -c 10 -r 50
+python launcher.py -n 1024random_diameter4test.edgelist -c 2.5 -r 50
+python launcher.py -n 1024random_diameter4test.edgelist -c 10 -r 50
+python launcher.py -n 1024random_diameter4test.edgelist -c inf -r 50
 
 echo All experiments completed. 
 pause
